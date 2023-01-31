a647eccf3ec6ac51c397bbffaef863b36a7318a7
Jason Anderson <anderson.jasonryan@gmail.com>
2023-01-31 16:46:05 -0700

    add linter

---
+++ src/components/Button/Button.types.ts
@@ -0,0 +1,9 @@
+import { type MouseEventHandler } from 'react'
+
+export interface ButtonProps {
+  text?: string
+  primary?: boolean
+  disabled?: boolean
+  size?: 'small' | 'medium' | 'large'
+  onClick?: MouseEventHandler<HTMLButtonElement>
+}
