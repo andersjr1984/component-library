af0e92daa99f25a6e6a58c4939d4a91c8a979f1b
Jason Anderson <anderson.jasonryan@gmail.com>
2023-01-31 19:55:04 -0700

    finalize build config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,6 +5,7 @@ import { terser } from 'rollup-plugin-terser'
 import external from 'rollup-plugin-peer-deps-external'
 import postcss from 'rollup-plugin-postcss'
 import { createRequire } from 'node:module'
+import dts from 'rollup-plugin-dts'
 const require = createRequire(import.meta.url)
 const packageJson = require('./package.json')
 
@@ -16,7 +17,12 @@ export default [
         file: packageJson.main,
         format: 'cjs',
         sourcemap: true,
-        name: 'react-ts-lib'
+        name: 'react-lib'
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true
       }
     ],
     plugins: [
@@ -27,5 +33,11 @@ export default [
       postcss(),
       terser()
     ]
+  },
+  {
+    input: 'dist/esm/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    external: [/\.css$/],
+    plugins: [dts()]
   }
 ]
